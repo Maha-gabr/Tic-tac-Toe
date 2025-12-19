48f234e34b721c05957215fd94c90d6ebd3986a5
Maha-Gabr <gabrmaha0@gmail.com>
2025-12-19 21:34:47 +0300

    initial project

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color blackColor = Color(0xff242527);
+  static final Color whiteColor = Color(0xffFFFFFF);
+  static final Color blueColor = Color(0xff3A7BD5);
+  static final Color lightBlueColor = Color(0xff00D2FF);
+}
